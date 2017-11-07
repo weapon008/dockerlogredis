d3a0c647cbc86b99d2d52755f8bb2bc575ce6db3
weipeng <624435565@qq.com>
2017-11-07 13:15:06 +0800

    Signed-off-by: weipeng <624435565@qq.com>

--- driver.go
+++ driver.go
@@ -143,6 +143,7 @@ func (d *driver) ReadLogs(info logger.Info, config logger.ReadConfig) (io.ReadCl
 			select {
 			case msg, ok := <-watcher.Msg:
 				if !ok {
+					log.Println(`!!!!!!!!!!!666`, err.Error())
 					w.Close()
 					return
 				}
