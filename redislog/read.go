7f4ce1fb81b2585b4a19efa054b34d106b3f98a7
weipeng <624435565@qq.com>
2017-11-07 11:30:33 +0800

    Signed-off-by: weipeng <624435565@qq.com>

--- redislog/read.go
+++ redislog/read.go
@@ -1,17 +1,24 @@
 package redislog
 
 import (
+	"log"
+
 	"github.com/docker/docker/daemon/logger"
 )
 
 func (rl *Redislog) ReadLogs(cfg logger.ReadConfig) *logger.LogWatcher {
 	logWatcher := logger.NewLogWatcher()
+	log.Println(`!!!!!!!!!!111`, cfg.Follow)
 	if cfg.Follow {
 		rl.bus.Sub(GetGUID(), logWatcher)
 	} else {
 		go func() {
+			log.Println(`!!!!!!!!!!222`)
 			msgs := rl.rb.Tail(cfg.Tail, cfg.Since)
-			for _, msg := range msgs {
+			i = 0
+			for {
+				log.Println(`!!!!!!!!!!333`, i)
+				msg := msgs[i]
 				select {
 				case <-logWatcher.WatchClose():
 					return
